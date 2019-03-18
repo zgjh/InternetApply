03448412fa883f12207e6e1140fc921010c8ccfb
zgjh <37134125+zgjh@users.noreply.github.com>
2019-03-18 18:58:53 +0800

    20190318 其中注目

--- Ch05/Ch5_3_1.php
+++ Ch05/Ch5_3_1.php
@@ -5,14 +5,18 @@
 <title>Ch5_3_1.php</title>
 </head>
 <body>
-<?php 
+<form name="form6" method="get" action="Ch5_3_1.php">
+請輸入次數:<input type="text" name="times" > </br>
+<input type="submit" value='送出'>
+<input type="reset" value='重寫'></br>
+<?php   
 $total = 0;   // 指定變數值
 // for遞增迴圈
-for ( $i = 1; $i <= 10; $i++ ) {
+for ( $i = 1; $i <= $_GET['times']; $i++ ) {
    print "|". $i;
    $total += $i;
 }
-print "|<br/>for遞增迴圈從1加到10=".$total."<br/>";
+print "|<br/>for遞增迴圈從1加到".$_GET['times']."=".$total."<br/>";
 $total = 0;   // 重設變數值
 // for遞減迴圈
 for ( $i = 60; $i >= 20; $i-- ) $total += $i;
